19ecd3cc57a19e813f263a22d68534ddd0373750
MacNFox <59699409+MacNFox@users.noreply.github.com>
2020-02-10 22:37:24 -0500

    Add files via upload
    Bonjour Jean-Hugues, 
    Est-ce possible de se rencontrer à la fin du cours de cette semaine afin de discuter de ce travail ? J'ai eu énormément de difficultés, je ne comprends pas pourquoi je n'arrive pas à saisir la logique de Python et m'en rappeler.
    Merci,
    David

---
+++ devoir2.py
@@ -0,0 +1,91 @@
+# coding: utf-8
+# Importation des fichiers CSV, JSON et REQUESTS
+
+import csv
+import json
+import requests
+
+fichier = "lobby.csv"
+url = "http://jhroy.ca/uqam/lobby.json"
+
+# Qui suis-je ?
+entetes = {
+    "User-Agent":"David Masse - 438/ 837-9870: requête envoyéee dans le cadre d'un cours de journalisme à l'UQAM",
+    "From":"davidmasseyt@hotmail.com"
+}
+
+# req = requests.get(url,headers=entetes)
+# print(req)
+
+# Est-ce que ça marche?
+# req = requests.get(fichier, headers=entetes)
+# print (req)
+# Tout est correct, j'obtiens la combinaison gangnante [200]
+
+if req.status_code == 200:
+     print("bonheur d'occasion")
+
+# Mes raccourcis
+
+n = 0
+board = []
+
+# Mes informations de boucle |>o<|
+# Création d'une boucle, afin de tout extraire 
+
+for number in range[0,72001]:
+    n+=1
+    comm=req.json()
+    req = requests.get(url, headers=entetes)
+    info.append(n)
+    info.append(nb)
+    # Mes conditions et mes valeurs qui sont à afficher
+    if "limat" in comm["registre"][0][1][0]["objet"]:
+        # Création de ma liste 1
+       liste1 = {
+           'num' : comm["registre"][0][0]["client_org_corp_num"],
+           'nom' : comm["registre"][0][0]["fr_client_org_corp_nm"],
+           'name' : comm["registre"][0][0]["en_client_org_corp_nm"],
+           'date' : comm["registre"][0][0]["date_comm"],
+           'sujet principal' : comm["registre"][0][1][0]["objet"],
+           'sujet autre' : comm["registre"][0][1][0]["objet_autre"],
+           'institution' : comm["registre"][0][2][0]["institution"],
+           }
+    elif "limat" in comm["registre"][0][1][1]["objet"]
+    # Création de ma liste 2 
+          liste2 = {
+           'num' : comm["registre"][0][0]["client_org_corp_num"],
+           'nom' : comm["registre"][0][0]["fr_client_org_corp_nm"],
+           'name' : comm["registre"][0][0]["en_client_org_corp_nm"],
+           'date' : comm["registre"][0][0]["date_comm"],
+           'sujet principal' : comm["registre"][0][1][1]["objet"],
+           'sujet autre' : comm["registre"][0][1][1]["objet_autre"],
+           'institution' : comm["registre"][0][2][0]["institution"],
+           }
+    elif "limat" in comm ["registre"][0][1][2]["objet"]
+    # Création de ma liste 3
+        liste3 = {
+           'num' : comm["registre"][0][0]["client_org_corp_num"],
+           'nom' : comm["registre"][0][0]["fr_client_org_corp_nm"],
+           'name' : comm["registre"][0][0]["en_client_org_corp_nm"],
+           'date' : comm["registre"][0][0]["date_comm"],
+           'sujet principal' : comm["registre"][0][1][2]["objet"],
+           'sujet autre' : comm["registre"][0][1][2]["objet_autre"],
+           'institution' : comm["registre"][0][2][0]["institution"],
+           }
+# Je viens clore ma boucle en demandant à Python d'exporter mes informations 
+        board.append(liste1)
+        board.append(liste2)
+        board.append(liste3)
+        dead = open(fichier,"a")
+        obies = csv.writer(dead)
+        obies.writerow(board)
+
+# J'ai une erreur de syntaxe à la ligne 54, mais je ne trouve pas l'erreur,
+# j'en suis désolé. 
+
+# Notes pour ne pas tout réécrire si je modifie de quoi entre-temps
+# print (obj["registre"][0][0]["fr_client_org_corp_nm"])
+# print (obj["registre"][0][0]["en_client_org_corp_nm"])
+# print (obj["registre"][0][0]["client_org_corp_num"])
+# num = obj["registre"][0][0]["client_org_corp_num"])
